4320764fa6de7c032211386355031d59729c4cd8
malrefai <malrefai@nyit.edu>
2018-02-23 16:35:17 +0200

    Add tests for model associations and fields

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Item, type: :model do
   context "validations for" do
     it { should be_mongoid_document }
     it { should have_timestamps }
+    it { should have_fields(:name, :done) }
+    it { should be_embedded_in(:todo) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:done) }
+
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Todo, type: :model do
   context "validations for" do
     it { should be_mongoid_document }
     it { should have_timestamps }
+    it { should have_fields(:title, :created_by) }
+    it { should embed_many(:items) }
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:created_by) }
   end
