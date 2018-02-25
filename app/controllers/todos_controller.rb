9f66bf1a86e712ed2cf76e7a96becdb263745b70
malrefai <malrefai@nyit.edu>
2018-02-25 22:05:59 +0200

    Create tests and implement todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,39 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: %i[show update destroy]
+
+  def index
+    @todos = Todo.all
+    json_response(@todos)
+  end
+
+  def show
+    json_response(@todo)
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  # strong params
+  def todo_params
+    # white list params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 end
