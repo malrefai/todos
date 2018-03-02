cd97ed190943efe74ecd8f2d1696cb7c28d5420c
malrefai <malrefai@nyit.edu>
2018-03-02 08:48:33 +0200

    Add destroy dependent feature for items in todo model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -7,7 +7,7 @@ class Todo
   field :created_by, type: String
 
   # associations
-  has_many :items
+  has_many :items, dependent: :destroy
 
   # validations
   validates_presence_of :title, :created_by
