e5be9fd4268708aeef7fd913d26f955a42c65a5f
malrefai <malrefai@nyit.edu>
2018-03-31 12:36:27 +0300

    Add set_todo method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  def initialize
+    @errors = []
+  end
+
+  private
+
+  def set_todo(todo_id = :todo_id)
+    @todo ||= Todo.find_by(id: params[todo_id])
+    unless @todo
+      @errors << I18n.t("mongoid.errors.models.todo.unknown")
+      json_response(@errors, :not_found)
+    end
+  end
 end
