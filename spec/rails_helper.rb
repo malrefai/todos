d8bf42c2bb620afac07665c3da8bf76d7f2e1825
Mohammad Alrefai <malrefai@nyit.edu>
2018-06-22 02:08:17 +0300

    Configure rspec and GC

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
@@ -76,4 +76,12 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+  config.after(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+
+  config.before(:each) { GC.disable }
+  config.after(:each) { GC.enable }
 end
